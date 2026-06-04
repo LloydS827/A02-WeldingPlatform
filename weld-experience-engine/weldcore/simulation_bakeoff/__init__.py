4958a1faf93f2b6aa7609a0672f29b9e90a1092e
LloydSun <sunguangji827@hotmail.com>
2026-06-05 00:30:08 +0800

    feat: add weld skill unit simulation task specs

--- weld-experience-engine/weldcore/simulation_bakeoff/__init__.py
+++ weld-experience-engine/weldcore/simulation_bakeoff/__init__.py
@@ -5,11 +5,14 @@ from .model import (
     SimulationTaskSpec,
     SimulatorAdapterResult,
 )
+from .task_specs import DEFAULT_SIMULATION_TASK_SPECS, default_simulation_task_specs
 
 __all__ = [
+    "DEFAULT_SIMULATION_TASK_SPECS",
     "BakeoffScorecard",
     "SimulationEvidenceBundle",
     "SimulationPathPoint",
     "SimulationTaskSpec",
     "SimulatorAdapterResult",
+    "default_simulation_task_specs",
 ]
