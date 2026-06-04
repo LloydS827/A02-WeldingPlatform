a5a8aa4917ca88b84fddbe2d0093d0d13847465e
LloydSun <sunguangji827@hotmail.com>
2026-06-04 21:44:57 +0800

    feat: add weld skill unit boundary

---
+++ weld-experience-engine/weldcore/skill_unit/__init__.py
@@ -0,0 +1,17 @@
+from .unit import (
+    CORNER_HORIZONTAL_TRANSITION,
+    DEFAULT_WELD_SKILL_UNITS,
+    LONG_STRAIGHT_HORIZONTAL_TRACKING,
+    U_SEAM_VERTICAL_EXTENSION,
+    WeldSkillUnit,
+    default_weld_skill_units,
+)
+
+__all__ = [
+    "CORNER_HORIZONTAL_TRANSITION",
+    "DEFAULT_WELD_SKILL_UNITS",
+    "LONG_STRAIGHT_HORIZONTAL_TRACKING",
+    "U_SEAM_VERTICAL_EXTENSION",
+    "WeldSkillUnit",
+    "default_weld_skill_units",
+]
